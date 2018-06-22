ff2b22a4783cbff26291ba3ab72c204c77df9ac5
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2018-06-22 13:54:48 +0530

    Add checkstyle issue fixes

--- tests/ballerina-test/src/test/java/org/ballerinalang/test/jwt/JwtTest.java
+++ tests/ballerina-test/src/test/java/org/ballerinalang/test/jwt/JwtTest.java
@@ -47,11 +47,12 @@ public class JwtTest {
     public void setup() throws Exception {
         keyStorePath = getClass().getClassLoader().getResource(
                 "datafiles/security/keyStore/ballerinaKeystore.p12").getPath();
-        trustStorePath = getClass().getClassLoader().getResource(
-                "datafiles/security/keyStore/ballerinaTruststore.p12").getPath();
+        trustStorePath = getClass().getClassLoader().getResource("datafiles/security/keyStore/ballerinaTruststore.p12")
+                .getPath();
         resourceRoot = new File(getClass().getProtectionDomain().getCodeSource().
                 getLocation().getPath()).getAbsolutePath();
-        resourceRoot = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath()).getAbsolutePath();
+        resourceRoot = new File(getClass().getProtectionDomain().getCodeSource().getLocation().getPath())
+                .getAbsolutePath();
         Path sourceRoot = Paths.get(resourceRoot, "test-src", "jwt");
         compileResult = BCompileUtil.compile(sourceRoot.resolve("jwt-test.bal").toString());
     }
@@ -70,5 +71,4 @@ public class JwtTest {
         BValue[] returns = BRunUtil.invoke(compileResult, "testValidateJwt", inputBValues);
         Assert.assertTrue((returns[0]) instanceof BBoolean);
     }
-
 }
