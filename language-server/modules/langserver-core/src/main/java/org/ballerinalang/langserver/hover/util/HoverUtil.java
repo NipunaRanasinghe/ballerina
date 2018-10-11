6d95150b8a0775432a95fec3364c62ffb497fa6a
Nipuna Ranasinghe <nipunaranasinghe93@gmail.com>
2018-10-11 12:03:22 +0530

    Add hover fix

--- language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/hover/util/HoverUtil.java
+++ language-server/modules/langserver-core/src/main/java/org/ballerinalang/langserver/hover/util/HoverUtil.java
@@ -177,7 +177,7 @@ public class HoverUtil {
         } else {
             hover = new Hover();
             List<Either<String, MarkedString>> contents = new ArrayList<>();
-            contents.add(Either.forLeft(""));
+            contents.add(Either.forRight(new MarkedString("","")));
             hover.setContents(contents);
         }
         return hover;
@@ -248,7 +248,7 @@ public class HoverUtil {
         }
 
         List<Either<String, MarkedString>> contents = new ArrayList<>();
-        contents.add(Either.forLeft(content.toString()));
+        contents.add(Either.forRight(new MarkedString("",content.toString())));
         hover.setContents(contents);
 
         return hover;
@@ -297,7 +297,7 @@ public class HoverUtil {
     private static Hover getDefaultHoverObject() {
         Hover hover = new Hover();
         List<Either<String, MarkedString>> contents = new ArrayList<>();
-        contents.add(Either.forLeft(""));
+        contents.add(Either.forRight(new MarkedString("","")));
         hover.setContents(contents);
 
         return hover;
